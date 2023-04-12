58feaf18447d91996891e077895675675da31983
Kanaru Sato <kanakanarunrun@gmail.com>
2023-04-13 01:10:43 +0900

    impl extension using wasm

--- vscode_extension/src/extension.ts
+++ vscode_extension/src/extension.ts
@@ -1,26 +1,34 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
+import { format_insert_queries_wasm } from '../insertfmt_core_wasm/insertfmt_core'
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    'insertfmt.fmtInsertQueries',
+    () => {
+      const editor = vscode.window.activeTextEditor
+      if (!editor) {
+        return
+      }
+      const text = editor.document.getText()
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "insertfmt" is now active!');
+      const result = format_insert_queries_wasm(text)
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('insertfmt.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from INSERTfmt!');
-	});
+      // replace the entire file
+      const firstLine = editor.document.lineAt(0)
+      const lastLine = editor.document.lineAt(editor.document.lineCount - 1)
+      const textRange = new vscode.Range(
+        firstLine.range.start,
+        lastLine.range.end
+      )
+      editor.edit((editBuilder) => {
+        editBuilder.replace(textRange, result)
+      })
+    }
+  )
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable)
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  return
+}
